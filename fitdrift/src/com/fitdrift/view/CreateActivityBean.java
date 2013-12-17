fa721d6b25b41e1618eb063fa9126c02b6eef309
mattheweallen <mattheweallen@gmail.com>
2013-12-17 04:46:12 -0600

    create activity.

---
+++ fitdrift/src/com/fitdrift/view/CreateActivityBean.java
@@ -0,0 +1,33 @@
+package com.fitdrift.view;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * This class supports the createactivity.xhtml
+ * 
+ * @author Matthew Allen
+ * @version 20131217
+ *
+ */
+@ManagedBean
+@RequestScoped
+public class CreateActivityBean {
+	private Date activityDate;
+
+	/**
+	 * @return the activityDate
+	 */
+	public Date getActivityDate() {
+		return activityDate;
+	}
+
+	/**
+	 * @param activityDate the activityDate to set
+	 */
+	public void setActivityDate(Date activityDate) {
+		this.activityDate = activityDate;
+	}
+}
