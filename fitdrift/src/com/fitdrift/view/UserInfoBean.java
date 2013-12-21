ab71279350b4fcf3a62e5ef562013bc091a7dab5
mattheweallen <mattheweallen@gmail.com>
2013-12-21 06:06:41 -0600

    fixed multiple calls and removed cruft.

--- fitdrift/src/com/fitdrift/view/UserInfoBean.java
+++ fitdrift/src/com/fitdrift/view/UserInfoBean.java
@@ -27,11 +27,6 @@ import java.util.Collection;
 public class UserInfoBean implements Serializable {
 	private static final long serialVersionUID = 5066455190365405534L;
 	private String username;
-	private String password;
-	private Boolean rememberMe;
-	private Boolean isAdmin;
-	private Boolean isGeneral;
-	private Long user_id;
 	private User user;
 	private String theme;
 	
@@ -52,20 +47,11 @@ public class UserInfoBean implements Serializable {
 		if(!isAuthenticated()) {
 			this.theme = "aristo";
 		} else if(getUser().getTheme() == null) {
-			AthleticgisFacade.updateUserTheme(getUser().getUser_id(), "bootstrap");
+			AthleticgisFacade.updateUserTheme(getUser().getUser_id(), "aristo");
 			this.theme = "aristo";
 		} else {
 			this.theme = getUser().getTheme();
 		}
-		
-		
-//		if (user == null) {
-//			this.theme = "bootstrap";
-//		} else if (user.getTheme() == null) {
-//			AthleticgisFacade.updateUserTheme(user.getUser_id(), "bootstrap");
-//		} else {
-//			this.theme = user.getTheme();
-//		}
 		return theme;
 	}
 
@@ -84,7 +70,7 @@ public class UserInfoBean implements Serializable {
 	 * @return the user_id
 	 */
 	public Long getUser_id() {
-		if (user_id == null) {
+		if (user == null) {
 			UserDetails userDetails = (UserDetails) SecurityContextHolder
 					.getContext().getAuthentication().getPrincipal();
 			username = userDetails.getUsername();
@@ -94,37 +80,13 @@ public class UserInfoBean implements Serializable {
 	}
 
 	public User getUser() {
-		if (user_id == null) {
+		if (user == null) {
 			UserDetails userDetails = (UserDetails) SecurityContextHolder
 					.getContext().getAuthentication().getPrincipal();
 			username = userDetails.getUsername();
 			user = AthleticgisFacade.findUserByUsername(username);
 		}
 		return user;
-	}
-
-	// /**
-	// * @param user_id the user_id to set
-	// */
-	// public void setUser_id(Long user_id) {
-	// this.user_id = user_id;
-	// }
-
-	// public Boolean getAdmin() {
-	// //return isAdmin;
-	//
-	// //hardcoded
-	// return "admin".equals(name);
-	// }
-	// public void setAdmin(Boolean admin) {
-	// this.admin = admin;
-	// }
-	public Boolean getIsGeneral() {
-		return isGeneral;
-	}
-
-	public void setIsGeneral(Boolean isGeneral) {
-		this.isGeneral = isGeneral;
 	}
 
 	/**
@@ -141,29 +103,6 @@ public class UserInfoBean implements Serializable {
 		return username;
 	}
 
-	// public void setName(String name) {
-	// this.name = name;
-	// }
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Boolean getRememberMe() {
-		return rememberMe;
-	}
-
-	public void setRememberMe(Boolean rememberMe) {
-		this.rememberMe = rememberMe;
-	}
-
-	public Boolean getLoggedIn() {
-		return username != null;
-	}
-
 	/**
 	 * @return the isAdmin
 	 */
@@ -178,13 +117,5 @@ public class UserInfoBean implements Serializable {
 			}
 		}
 		return false;
-	}
-
-	/**
-	 * @param isAdmin
-	 *            the isAdmin to set
-	 */
-	public void setIsAdmin(Boolean isAdmin) {
-		this.isAdmin = isAdmin;
 	}
 }
