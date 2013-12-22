5555aa00c143a6da17e801457761a60e4cca587d
mattheweallen <mattheweallen@gmail.com>
2013-12-22 06:29:13 -0600

    Military time validator.

---
+++ fitdrift/src/com/fitdrift/util/validate/MilitaryTimeValidator.java
@@ -0,0 +1,30 @@
+package com.fitdrift.util.validate;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import com.fitdrift.util.regex.Time24HoursValidator;
+ 
+/**
+ *  Validates military time.
+ * 
+ */
+@FacesValidator(value = "militaryTimeValidator")
+public class MilitaryTimeValidator implements Validator {
+	@Override
+    public void validate(FacesContext context, UIComponent component,
+            Object value) throws ValidatorException {
+        String time = String.valueOf(value);
+        boolean valid = (new Time24HoursValidator()).validate(time);
+        if (!valid) {
+            FacesMessage message = new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR, "Invalid military time.",
+                    "The time you entered is not a valid military time.");
+            throw new ValidatorException(message);
+        }
+    }
+}
