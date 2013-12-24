dd2df67c3d4ed9562e804f9c099d297b7c6cc0d5
mattheweallen <mattheweallen@gmail.com>
2013-12-24 07:20:51 -0600

    failed experiment with setActionListener

--- fitdrift/src/com/fitdrift/view/CreateActivityBean.java
+++ fitdrift/src/com/fitdrift/view/CreateActivityBean.java
@@ -6,10 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
-
 import com.fitdrift.domain.activity.MyMap;
 import com.fitdrift.domain.activity.MyMapMarker;
 import com.fitdrift.model.AthleticgisFacade;
@@ -26,141 +23,141 @@ import com.fitdrift.util.gis.GISCalculator;
 @ManagedBean
 @ViewScoped
 public class CreateActivityBean implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4050031415234475481L;
-	private Date activityDate;
-	private String activityType;
-	private boolean indoors;
-	private boolean useMyMap;
-	private MyMap selectedMap;
-	private String mapMapName;
-	private String distance;
-	
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 4050031415234475481L;
+        private Date activityDate;
+        private String activityType;
+        private boolean indoors;
+        private boolean useMyMap;
+        private MyMap selectedMap;
+        private String mapMapName;
+        private String distance;
+        
 
-	/**
-	 * @return the activityDate
-	 */
-	public Date getActivityDate() {
-		return activityDate;
-	}
+        /**
+         * @return the activityDate
+         */
+        public Date getActivityDate() {
+                return activityDate;
+        }
 
-	/**
-	 * @param activityDate the activityDate to set
-	 */
-	public void setActivityDate(Date activityDate) {
-		this.activityDate = activityDate;
-	}
-	
-	public String createActivity() {
-		return "createactivity";
-	}
+        /**
+         * @param activityDate the activityDate to set
+         */
+        public void setActivityDate(Date activityDate) {
+                this.activityDate = activityDate;
+        }
+        
+        public String createActivity() {
+                return "createactivity";
+        }
 
-	/**
-	 * @return the activityType
-	 */
-	public String getActivityType() {
-		return activityType;
-	}
+        /**
+         * @return the activityType
+         */
+        public String getActivityType() {
+                return activityType;
+        }
 
-	/**
-	 * @param activityType the activityType to set
-	 */
-	public void setActivityType(String activityType) {
-		this.activityType = activityType;
-	}
+        /**
+         * @param activityType the activityType to set
+         */
+        public void setActivityType(String activityType) {
+                this.activityType = activityType;
+        }
 
-	/**
-	 * @return the indoors
-	 */
-	public boolean isIndoors() {
-		return indoors;
-	}
+        /**
+         * @return the indoors
+         */
+        public boolean isIndoors() {
+                return indoors;
+        }
 
-	/**
-	 * @param indoors the indoors to set
-	 */
-	public void setIndoors(boolean indoors) {
-		this.indoors = indoors;
-	}
-	
-	public String addMyMap() {
-		
-		
-		if(selectedMap != null) {
-			System.out.println(selectedMap.getName());
-			this.mapMapName = selectedMap.getName();
-			
-			List<MyMapMarker> myMapMarkers = AthleticgisFacade
-					.findMyMapMarkersByMymap_id(selectedMap.getMymap_id());
-			
-			GISCalculator calc = new GISCalculator();
-			Double distance = calc.computeMarkerPathDistance(myMapMarkers) / 1000;
-			DecimalFormat df = new DecimalFormat("#.##");
-			
-			this.distance = df.format(distance); 
-		}
-		return null;
-	}
+        /**
+         * @param indoors the indoors to set
+         */
+        public void setIndoors(boolean indoors) {
+                this.indoors = indoors;
+        }
+        
+        public String addMyMap() {
+                
+                
+                if(selectedMap != null) {
+                        System.out.println(selectedMap.getName());
+                        this.mapMapName = selectedMap.getName();
+                        
+                        List<MyMapMarker> myMapMarkers = AthleticgisFacade
+                                        .findMyMapMarkersByMymap_id(selectedMap.getMymap_id());
+                        
+                        GISCalculator calc = new GISCalculator();
+                        Double distance = calc.computeMarkerPathDistance(myMapMarkers) / 1000;
+                        DecimalFormat df = new DecimalFormat("#.##");
+                        
+                        this.distance = df.format(distance); 
+                }
+                return null;
+        }
 
-	/**
-	 * @return the selectedMap
-	 */
-	public MyMap getSelectedMap() {
-		return selectedMap;
-	}
+        /**
+         * @return the selectedMap
+         */
+        public MyMap getSelectedMap() {
+                return selectedMap;
+        }
 
-	/**
-	 * @param selectedMap the selectedMap to set
-	 */
-	public void setSelectedMap(MyMap selectedMap) {
-		this.selectedMap = selectedMap;
-	}
+        /**
+         * @param selectedMap the selectedMap to set
+         */
+        public void setSelectedMap(MyMap selectedMap) {
+                this.selectedMap = selectedMap;
+        }
 
-	/**
-	 * @return the mapMapName
-	 */
-	public String getMapMapName() {
-		return mapMapName;
-	}
+        /**
+         * @return the mapMapName
+         */
+        public String getMapMapName() {
+                return mapMapName;
+        }
 
-	/**
-	 * @param mapMapName the mapMapName to set
-	 */
-	public void setMapMapName(String mapMapName) {
-		this.mapMapName = mapMapName;
-	}
+        /**
+         * @param mapMapName the mapMapName to set
+         */
+        public void setMapMapName(String mapMapName) {
+                this.mapMapName = mapMapName;
+        }
 
-	/**
-	 * @return the useMyMap
-	 */
-	public boolean isUseMyMap() {
-		return useMyMap;
-	}
+        /**
+         * @return the useMyMap
+         */
+        public boolean isUseMyMap() {
+                return useMyMap;
+        }
 
-	/**
-	 * @param useMyMap the useMyMap to set
-	 */
-	public void setUseMyMap(boolean useMyMap) {
-		this.useMyMap = useMyMap;
-	}
-	
-	public void useMyMapChecked() {
-		distance = null;
-	}
+        /**
+         * @param useMyMap the useMyMap to set
+         */
+        public void setUseMyMap(boolean useMyMap) {
+                this.useMyMap = useMyMap;
+        }
+        
+        public void useMyMapChecked() {
+                distance = null;
+        }
 
-	/**
-	 * @return the distance
-	 */
-	public String getDistance() {
-		return distance;
-	}
+        /**
+         * @return the distance
+         */
+        public String getDistance() {
+                return distance;
+        }
 
-	/**
-	 * @param distance the distance to set
-	 */
-	public void setDistance(String distance) {
-		this.distance = distance;
-	}
+        /**
+         * @param distance the distance to set
+         */
+        public void setDistance(String distance) {
+                this.distance = distance;
+        }
 }
--- fitdrift/src/com/fitdrift/view/ViewMyMapBean.java
+++ fitdrift/src/com/fitdrift/view/ViewMyMapBean.java
@@ -9,9 +9,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
-import javax.faces.component.html.HtmlInputText;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.model.map.DefaultMapModel;
@@ -33,87 +31,87 @@ import com.fitdrift.util.gis.GISCalculator;
 @ManagedBean
 @ViewScoped
 public class ViewMyMapBean implements Serializable {
-	private static final long serialVersionUID = -4430301657324644250L;
-	@ManagedProperty(value = "#{request.getParameter('mymap_id')}")
-	private String mymap_id;
-	private MapModel polylineModel = new DefaultMapModel();
-	private String myMapName;
+        private static final long serialVersionUID = -4430301657324644250L;
+        @ManagedProperty(value = "#{request.getParameter('mymap_id')}")
+        private String mymap_id;
+        private MapModel polylineModel = new DefaultMapModel();
+        private String myMapName;
 
-	@PostConstruct
-	public void initialize() {
-		Polyline polyline = new Polyline();
-		polyline.setStrokeWeight(2);
-		polyline.setStrokeColor("#FF0000");
-		polyline.setStrokeOpacity(1.0);
-		List<MyMapMarker> myMapMarkers = AthleticgisFacade
-				.findMyMapMarkersByMymap_id(Long.parseLong(mymap_id));
-		for (MyMapMarker m : myMapMarkers) {
-			polyline.getPaths().add(
-					new LatLng(m.getLatitude(), m.getLongitude()));
+        @PostConstruct
+        public void initialize() {
+                Polyline polyline = new Polyline();
+                polyline.setStrokeWeight(2);
+                polyline.setStrokeColor("#FF0000");
+                polyline.setStrokeOpacity(1.0);
+                List<MyMapMarker> myMapMarkers = AthleticgisFacade
+                                .findMyMapMarkersByMymap_id(Long.parseLong(mymap_id));
+                for (MyMapMarker m : myMapMarkers) {
+                        polyline.getPaths().add(
+                                        new LatLng(m.getLatitude(), m.getLongitude()));
 
-			// add markers
-			Marker marker = new Marker(new LatLng(m.getLatitude(),
-					m.getLongitude()), m.getName());
-			polylineModel.addOverlay(marker);
-		}
-		// GISCalculator g = new GISCalculator();
-		// System.out.println("Distance is " +
-		// g.computePathDistance(activityPoints));
+                        // add markers
+                        Marker marker = new Marker(new LatLng(m.getLatitude(),
+                                        m.getLongitude()), m.getName());
+                        polylineModel.addOverlay(marker);
+                }
+                // GISCalculator g = new GISCalculator();
+                // System.out.println("Distance is " +
+                // g.computePathDistance(activityPoints));
 
-		polylineModel.addOverlay(polyline);
+                polylineModel.addOverlay(polyline);
 
-		GISCalculator calc = new GISCalculator();
-		Double distance = calc.computeMarkerPathDistance(myMapMarkers) / 1000;
-		DecimalFormat df = new DecimalFormat("#.##");
-		addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Distance",
-				"Your total distance is " + df.format(distance) + " km."));
-	}
+                GISCalculator calc = new GISCalculator();
+                Double distance = calc.computeMarkerPathDistance(myMapMarkers) / 1000;
+                DecimalFormat df = new DecimalFormat("#.##");
+                addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Distance",
+                                "Your total distance is " + df.format(distance) + " km."));
+        }
 
-	public void addMessage(FacesMessage message) {
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	}
+        public void addMessage(FacesMessage message) {
+                FacesContext.getCurrentInstance().addMessage(null, message);
+        }
 
-	/**
-	 * @return the polylineModel
-	 */
-	public MapModel getPolylineModel() {
-		return polylineModel;
-	}
+        /**
+         * @return the polylineModel
+         */
+        public MapModel getPolylineModel() {
+                return polylineModel;
+        }
 
-	public String getMymap_id() {
-		return mymap_id;
-	}
+        public String getMymap_id() {
+                return mymap_id;
+        }
 
-	public void setMymap_id(String mymap_id) {
-		this.mymap_id = mymap_id;
-	}
+        public void setMymap_id(String mymap_id) {
+                this.mymap_id = mymap_id;
+        }
 
-	public String updateMyMap() {
-		Map<String, String> params = FacesContext.getCurrentInstance()
-				.getExternalContext().getRequestParameterMap();
-		String mId = params.get("mymap_id");
-		Long id = Long.parseLong(mId);
-		if (myMapName != null && myMapName.length() > 0) {
-			AthleticgisFacade.mergeMyMap(myMapName, id);
-		}
-		return "mymaps?faces-redirect=true";
-	}
+        public String updateMyMap() {
+                Map<String, String> params = FacesContext.getCurrentInstance()
+                                .getExternalContext().getRequestParameterMap();
+                String mId = params.get("mymap_id");
+                Long id = Long.parseLong(mId);
+                if (myMapName != null && myMapName.length() > 0) {
+                        AthleticgisFacade.mergeMyMap(myMapName, id);
+                }
+                return "mymaps?faces-redirect=true";
+        }
 
-	/**
-	 * @return the myMapName
-	 */
-	public String getMyMapName() {
-		if (mymap_id != null) {
-			myMapName = AthleticgisFacade.findMyMapById(Long.parseLong(mymap_id)).getName();
-		}
-		return myMapName;
-	}
+        /**
+         * @return the myMapName
+         */
+        public String getMyMapName() {
+                if (mymap_id != null) {
+                        myMapName = AthleticgisFacade.findMyMapById(Long.parseLong(mymap_id)).getName();
+                }
+                return myMapName;
+        }
 
-	/**
-	 * @param myMapName the myMapName to set
-	 */
-	public void setMyMapName(String myMapName) {
-		
-		this.myMapName = myMapName;
-	}
+        /**
+         * @param myMapName the myMapName to set
+         */
+        public void setMyMapName(String myMapName) {
+                
+                this.myMapName = myMapName;
+        }
 }
